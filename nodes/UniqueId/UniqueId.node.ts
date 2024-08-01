aa44375bcbebb75784d994d5d4b67eecb9dab90e
Steven Shen <sysheen@gmail.com>
2024-08-01 09:07:31 +0800

    fix installation

--- nodes/UniqueId/UniqueId.node.ts
+++ nodes/UniqueId/UniqueId.node.ts
@@ -5,8 +5,8 @@ import {
 	INodeTypeDescription,
 } from 'n8n-workflow';
 
-let counter = 0;
-let currentDateStr = '';
+// let counter = 0;
+// let currentDateStr = '';
 
 export class UniqueId implements INodeType {
 	description: INodeTypeDescription = {
@@ -25,6 +25,9 @@ export class UniqueId implements INodeType {
 		],
 	};
 
+	static counter = 0;
+	static currentDateStr = '';
+
 	// The execute method will go here
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const now = new Date();
@@ -34,16 +37,16 @@ export class UniqueId implements INodeType {
 		const hours = String(now.getHours()).padStart(2, '0');
 		const minutes = String(now.getMinutes()).padStart(2, '0');
 		const dateStr = `${year}${month}${day}${hours}${minutes}`;
-		if (dateStr !== currentDateStr) {
-			currentDateStr = dateStr;
-			counter = 0;
+		if (dateStr !== UniqueId.currentDateStr) {
+			UniqueId.currentDateStr = dateStr;
+			UniqueId.counter = 0;
 		}
-		const counterStr = String(counter).padStart(4, '0');
+		const counterStr = String(UniqueId.counter).padStart(4, '0');
 
 		const str = `${dateStr}${counterStr}`;
-		counter ++;
-		if (counter > 9999) {
-			counter = 0;
+		UniqueId.counter += 1;
+		if (UniqueId.counter > 9999) {
+			UniqueId.counter = 0;
 		}
 		return this.prepareOutputData([{ json: {
 			identity: str
